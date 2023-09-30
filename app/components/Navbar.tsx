cc51155ad7df65b562cbfd30f220473cf2614f3b
Hossam Cj <hossamcj@gmail.com>
2023-09-30 18:03:53 +0300

    Reading mardown with gray-matter

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,32 @@
 import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
 export default function Navbar() {
     return (
         <nav className="bg-slate-600 text-white p-4 sticky top-0 drop-shadow-xl z-10">
-            <div className=" prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
-                <Link
-                    href="/"
-                    className="text-white/90 no-underline hover:text-white"
-                >
-                    HossamCj
-                </Link>
+            <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+                <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+                    <Link
+                        href="/"
+                        className="text-white/90 no-underline hover:text-white"
+                    >
+                        HossamCj
+                    </Link>
+                </h1>
+                <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+                    <Link
+                        href="https://www.linkedin.com/in/hossam-hassan-mohamed-738420145/?locale=en_US"
+                        className="text-white/90 hover:text-white"
+                    >
+                        <FaLinkedin />
+                    </Link>
+                    <Link
+                        href="https://www.github.com/Hossamcj"
+                        className="text-white/90 hover:text-white"
+                    >
+                        <FaGithub />
+                    </Link>
+                </div>
             </div>
         </nav>
     );
---
+++ lib/posts_lib.tsx
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "blogposts");
+
+export function getStorePstsData() {
+    // Get file name under /posts
+    const fileNames = fs.readdirSync(postsDirectory);
+    const allPostsData = fileNames.map((fileName) => {
+        // Remove ".md" fromfile name to get id
+        const id = fileName.replace(/\.md$/, "");
+
+        // Read mardown files as string
+        const fullPath = path.join(postsDirectory, fileName);
+        const fileContents = fs.readFileSync(fullPath, "utf8");
+
+        // User gray-matter to parse the post metadata section
+        const matterResult = matter(fileContents);
+        const blogPost: BlogPost = {
+            id,
+            title: matterResult.data.title,
+            date: matterResult.data.date,
+        };
+
+        // Combine the data with the id
+        return blogPost;
+    });
+
+    // Sort posts by data
+    return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
