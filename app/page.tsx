cee688dd31fc9d3beeeaa891b032f64848443738
Hossam Cj <hossamcj@gmail.com>
2023-09-30 18:43:40 +0300

    change the font-size of the <p> main section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import Image from "next/image";
-
 import { Fragment } from "react";
+import Posts from "./components/Posts";
 
 /* eslint-disable react/no-unescaped-entities */
 export default function Home() {
     return (
         <Fragment>
             <main className="px-6 mx-auto">
-                <p className="mt-12 mb-12 text-3xl text-center text-white">
+                <p className="mt-12 mb-12 text-2xl text-center text-white">
                     Hello and Welcome 👋&nbsp;
                     <span className="whitespace-nowrap">
                         I'm <span className="font-bold">Hossam Cj</span>
                     </span>
                 </p>
+                <Posts />
             </main>
         </Fragment>
     );
