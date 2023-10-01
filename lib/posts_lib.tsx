d454cfc59498fd5c918a219867713044a213cb3c
Hossam Cj <hossamcj@gmail.com>
2023-10-01 15:43:19 +0300

    Create a custom error page

--- lib/posts_lib.tsx
+++ lib/posts_lib.tsx
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import {remark} from "remark";
+import html from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "blogposts");
 
@@ -29,4 +31,28 @@ export function getSortedPostsData() {
 
     // Sort posts by data
     return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
+
+export async function getPostData(id: string) {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // User gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    const processedContent = await remark()
+        .use(html)
+        .process(matterResult.content);
+
+    const contentHTML = processedContent.toString();
+
+    const blogPostWithHTML: BlogPost & {contentHTML: string} = {
+        id,
+        title: matterResult.data.title,
+        date: matterResult.data.date,
+        contentHTML,
+    };
+
+    // Combine the data with the id
+    return blogPostWithHTML;
 }
